103867db16046a50164b6178afdeff1f43a1bde8
이희성 <jini4166@gmail.com>
2023-07-27 17:37:53 +0900

    (2023.7.27) 변수의 선언

--- Unity Tutorial/Assets/Script/Car.cs
+++ Unity Tutorial/Assets/Script/Car.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    
+    #region 변수
+    //데이처를 저장 할 수 있는 메모리
+
+    char character = 'H';
+
+    int num = 10000;
+
+    float pi = 3.141592f;
+
+    private void Start() {
+        Debug.Log(character);
+        Debug.Log(num);
+        Debug.Log(pi);
+    }
+
+
+    #endregion
+
+
 }
