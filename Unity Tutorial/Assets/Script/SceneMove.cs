cd942b792332dcaefb2ffcb1df3b968429d4986a
이희성 <jini4166@gmail.com>
2023-08-10 14:34:22 +0900

    (2023.8.10) UI 사용방법 (복구)

---
+++ Unity Tutorial/Assets/Script/SceneMove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    public void Move(){
+        SceneManager.LoadScene(1);
+    }
+}
